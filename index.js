128e55b87624c1e7f7b6226106fee96b81489c6b
floopus <yellowissuchanoverratedcolor@gmail.com>
2026-08-30 13:53:39 +0200

    Initial commit

--- index.js
+++ index.js
@@ -8,9 +8,25 @@ const app = new App({
     socketMode: true,
 });
 
+function assembleLetterDict(input) {
+    input = input.toLowerCase();
+    const letterDict = {};
+    for (const char of input) {
+        if (/[a-z]/.test(char)) {
+            if (letterDict[char]) {
+                letterDict[char]++;
+            }
+            else {
+                letterDict[char] = 1;
+            }
+        }
+    }
+    return letterDict;
+}
+
 app.command("/yellow-help", async ({ ack, say }) => {
     await ack();
-    await say("Hi I can do /help, /yellow-ping and yea thats it");
+    await say("Hi I can do /help, /yellow-ping /yellow-count-letters and yea thats it");
 });
 
 app.command("/yellow-ping", async ({ command, ack, say }) => {
@@ -18,6 +34,18 @@ app.command("/yellow-ping", async ({ command, ack, say }) => {
     await ack();
     const latency = Date.now() - start;
     await say(`Pong! 🏓 (${latency}ms)`);
+});
+
+app.command("/yellow-count-letters", async ({ command, ack, say }) => {
+    await ack();
+    const d = assembleLetterDict(command.text);
+    if (Object.keys(d).length === 0) {
+        await say("No letters :(");
+    } else if (Object.keys(d).length === 26) {
+        await say(`All 26 letters are present! yay! most common letter: ${Object.keys(d).reduce((a, b) => d[a] > d[b] ? a : b)}`);
+    } else {
+        await say(`Letter count: ${Object.keys(d).length}, most common letter: ${Object.keys(d).reduce((a, b) => d[a] > d[b] ? a : b)}`);
+    }
 });
 
 (async () => {
